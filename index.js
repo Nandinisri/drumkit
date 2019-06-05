a05a4e857b63f1a3d96be87fdf99672b3b0be730
Nandini Srivastava <44871328+Nandinisri@users.noreply.github.com>
2019-06-05 10:14:17 +0530

    udated index.js file

--- index.js
+++ index.js
@@ -13,31 +13,31 @@ document.addEventListener("keypress",function(e){
 function palySound(param){
   switch (param) {
     case "w":
-      var x = new Audio("sounds/crash.mp3");
+      var x = new Audio("crash.mp3");
       x.play();
       break;
     case "a":
-      var x = new Audio("sounds/kick-bass.mp3");
+      var x = new Audio("kick-bass.mp3");
       x.play();
       break;
     case "s":
-      var x = new Audio("sounds/snare.mp3");
+      var x = new Audio("snare.mp3");
       x.play();
       break;
     case "d":
-      var x = new Audio("sounds/tom-1.mp3");
+      var x = new Audio("tom-1.mp3");
       x.play();
       break;
     case "j":
-      var x = new Audio("sounds/tom-2.mp3");
+      var x = new Audio("tom-2.mp3");
       x.play();
       break;
     case "k":
-      var x = new Audio("sounds/tom-3.mp3");
+      var x = new Audio("tom-3.mp3");
       x.play();
       break;
     case "l":
-      var x = new Audio("sounds/tom-4.mp3");
+      var x = new Audio("tom-4.mp3");
       x.play();
       break;
     default:
